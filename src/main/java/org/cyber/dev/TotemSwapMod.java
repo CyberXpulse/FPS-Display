19f50ddf82e939a80417b0b16274415c28869ae5
CyberXpulse <157131142+CyberXpulse@users.noreply.github.com>
2025-02-22 16:58:51 +0530

    Update TotemSwapMod.java

--- src/main/java/org/cyber/dev/TotemSwapMod.java
+++ src/main/java/org/cyber/dev/TotemSwapMod.java
@@ -1,1 +1,16 @@
+package org.cyber.dev;
 
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TotemSwapMod implements ModInitializer {
+    public static final String MOD_ID = "totemswap";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("TotemSwapMod initialized!");
+        TotemKeybindHandler.register();
+    }
+}
