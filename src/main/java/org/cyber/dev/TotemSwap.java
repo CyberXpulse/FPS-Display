7f41e3c885efb1ea546102e803a50aef145205fd
CyberXpulse <157131142+CyberXpulse@users.noreply.github.com>
2025-03-09 09:39:33 +0530

    Update and rename TotemSwapMod.java to TotemSwap.java

---
+++ src/main/java/org/cyber/dev/TotemSwap.java
@@ -0,0 +1,10 @@
+package org.cyber.dev;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TotemSwap implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        System.out.println("[TotemSwap] Mod Initialized");
+    }
+}
